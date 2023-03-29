f7635f41b5bfd0730f02002399441f4e2d2b1f32
Chris Cullinane <chriscullinane@protonmail.com>
2023-03-29 09:14:03 -0800

    add second array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,8 @@ array = ["Zachary", "Indira", "Leah", "Kayt", "Vidya"]
 p array[2]
 p array[4]
 p array[0]
+array_2 = ["Alaska", "Texas", "Louisiana", "Colorado", "California"]
+p array_2.shift
+p array_2.shift
+p array_2.shift
+p array_2.shift
